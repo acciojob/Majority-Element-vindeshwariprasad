a660a001d128bc38ea647f0c5385b68608fb3e9a
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-04-05 00:30:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function okay(a) {
+	let aa = a[0];
+	let c = 1;
+	for (let i = 1;i<a.length;i++){
+		if (aa===a[i]){
+			c++;
+		}
+		else{
+			c--;
+		}
+		if (c===0){
+			aa = a[i];
+			c = 1;
+		}
+	}
+	
+	return aa;
+}
+
+okay(a);
